265df79b5dfd58a906959694d96dffe9eca93d5d
SkylarGoodwin <JamesSkylarGoodwin@gmail.com>
2024-02-25 18:09:14 -0600

    first day in mentorship program

--- obj/Debug/net7.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net7.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+113e219bdd6fca91389d9e42a4d012604dadcc3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
